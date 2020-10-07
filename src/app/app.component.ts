47fb2b1f0a408ede5a8cc90d5addd881b411a228
tjava <mustyytee@gmail.com>
2020-10-07 19:42:40 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyBuWcBJ3wxEl0noyU1EdzZ1-fjCfEWVDz0',
+  databaseURL: 'https://chatapp-169a6.firebaseio.com'
+};
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chatapp';
+
+  constructor() {
+    firebase.initializeApp(config);
+  }
 }
